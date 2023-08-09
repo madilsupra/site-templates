6df2b8f167408d313fdd1054ba740a067d27d884
Madil Supra <67541796+madilsupra@users.noreply.github.com>
2023-08-10 00:26:05 +0530

    Create javascript-unsupported.js

---
+++ kia-cybersecurity.blogspot.com/javascript-unsupported.js
@@ -0,0 +1,10 @@
+js-unsupported-terminal {
+    color: white;
+    background-color:#012922;
+    border:1px solid silver;
+    border-radius:5px;
+    max-width:100%;
+    text-align:left;
+    white-space:break-spaces;
+    word-break:keep-all;
+}
