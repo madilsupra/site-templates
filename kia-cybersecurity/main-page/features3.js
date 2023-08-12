44f4afdad6836eeb7e094f75d06e50c84e8fc434
Madil Supra <67541796+madilsupra@users.noreply.github.com>
2023-08-12 17:38:30 +0530

    Update features3.js

--- kia-cybersecurity/main-page/features3.js
+++ kia-cybersecurity/main-page/features3.js
@@ -6,9 +6,13 @@ let pageContainers  = document.querySelectorAll("div[class='post-outer-container
 let postContainers1 = document.querySelectorAll("div[class='widget FeaturedPost']");
 let postContainers2 = document.querySelectorAll("article[class='post-outer-container']");
 let maxContainerLength = Math.max(pageContainers.length, postContainers1.length, postContainers2.length);
+let style = "background-color: rgb(1, 26, 22); border: 2px solid rgb(0, 102, 102); border-radius: 10px; opacity: 0.92; box-shadow: rgb(0, 102, 102) 0px 0px 10px 2px;";
 
 for(var i=0; i < maxContainerLength; i++){
-  if (pageContainers[i]){ pageContainers[i].className += " box-shadow-animation";}
+  if (pageContainers[i]){ 
+    pageContainers[i].className += " box-shadow-animation";
+    psgeContainers[i].style = style;
+  }
   if (postContainers1[i]){ postContainers1[i].className += " box-shadow-animation";}
   if (postContainers2[i]){
     if (location.pathname == '/'){
