3ed7f6dfc24d1cd36fc6faffe62979bf4e91c78a
Madil Supra <67541796+madilsupra@users.noreply.github.com>
2023-08-12 17:10:12 +0530

    Update and rename features2.js to features3.js

--- kia-cybersecurity/main-page/features3.js
+++ kia-cybersecurity/main-page/features3.js
@@ -2,6 +2,21 @@
 let title = document.querySelector("div[class='header-widget']");
 title.childNodes[1].innerHTML = "\n<h1>\nCybersecurity <sup><em style='font-size:80%;font-family:Oxygen;'>Know It All</em></sup>\n</h1>\n";
 
+let pageContainers  = document.querySelectorAll("div[class='post-outer-container']");
+let postContainers1 = document.querySelectorAll("div[class='widget FeaturedPost']");
+let postContainers2 = document.querySelectorAll("article[class='post-outer-container']");
+let maxContainerLength = Math.max(pageContainers.length, postContainers1.length, postContainers2.length);
+
+for(var i=0; i < maxContainerLength; i++){
+  if (pageContainers[i]){ pageContainers[i].className += " box-shadow-animation";}
+  if (postContainers1[i]){ postContainers1[i].className += " box-shadow-animation";}
+  if (postContainers2[i]){
+    if (location.pathname == '/'){
+      postContainers2[i].className += " box-shadow-animation";
+    }
+  }
+}
+
 /* Customize panel menu */
 let panelMenu = document.getElementsByTagName("aside")[0];
 panelMenu.style = "background-color:#041515;opacity:92%;font-family:Oxygen;";
